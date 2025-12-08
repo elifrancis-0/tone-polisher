9233f8b4ca67ba2d4a41f989c6ad2aa8885e4073
Elijah Inocent-Francs <elifrancis0@Mac.lan>
2025-12-07 22:25:28 -0500

    Added interaction button and uppercase logic

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 st.title("Tone Polisher")
-st.write("Fixed my grammar.")
+draft = st.text_area("Enter a rough draft")
+
+if st.button("Polish Tone"):
+    st.write(draft.upper())
